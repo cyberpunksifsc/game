45d206b297702c760ba61bf71609b0dbe90857a5
nicolas4pitz <nicolas2004pitz@gmail.com>
2026-09-01 22:01:35 -0300

    chore: inital development

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 import pyxel
 
-pyxel.init(160, 160, "Game")
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+class App:
+    def __init__(self):
+        pyxel.init(160, 160, "Game")
+        self.x = 0
+        self.mapa()
+        pyxel.run(self.update, self.draw)
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.blt(10, 10, 0, 0, 0, 120, 120, 0)
+    def mapa(self):
+        pyxel.images[0].load(0, 0, "../assets/BackGround/city1/6.png")
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
 
-pyxel.run(update, draw)
+App()
